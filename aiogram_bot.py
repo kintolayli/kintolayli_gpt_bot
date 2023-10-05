ea9c68ac5c63ab14ea61a315ade2b4559b79eed9
kintolayli <golkiper-leo@yandex.ru>
2023-10-05 12:11:27 +0300

    wip

--- aiogram_bot.py
+++ aiogram_bot.py
@@ -1,26 +1,24 @@
-import os
-import datetime
-
-from dotenv import load_dotenv
-
 import asyncio
+import datetime
 import logging
+import os
 import sys
-from os import getenv
-import aioschedule
-
+from aiogram import Bot, Dispatcher, types, exceptions
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.filters.command import Command
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
-from aiogram.filters.command import Command
-from aiogram import Bot, Dispatcher, types
+from dotenv import load_dotenv
+from os import getenv
 
 from gpt4_interface import gpt4_interface
-from literals import WELCOME_MESSAGE
+from literals import WELCOME_MESSAGE, START_SUMMARIZE_MESSAGE
 
-DEBUG = False
+DEBUG = True
 TIME_TO_PRINT = '21:40:00'
+MESSAGE_TIME = datetime.time(hour=19, minute=36, second=0)
+TIME_INTERVAL_MIN = 5
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
@@ -31,23 +29,35 @@ participants = {}
 participants_messages = []
 my_list = []
 
-ID_USER = 38743669
-ID_USER = 6518329630
 
-MESSAGE_TIME = datetime.time(hour=19, minute=36, second=0)
-MESSAGE_SUMMARIZATION_TEXT = ("Пожалуйста, суммаризируй и выдели главное из "
-                              "этих сообщений: ")
+
+async def summarize(message: types.Message, len_messages=None):
+    if len(participants_messages) > 5:
+        if len_messages is None:
+            len_messages = len(participants_messages) * -1
+        else:
+            len_messages *= -1
+
+        print(f"{len_messages=}")
+
+        last_participants_messages = participants_messages[len_messages::]
+        participants_messages_str = ' '.join(map(str, last_participants_messages))
+        summarization = gpt4_interface(
+            f"{START_SUMMARIZE_MESSAGE} {participants_messages_str}")
+
+        await bot.send_message(message.chat.id, f"Данные для суммаризации:{last_participants_messages}\n\nОтвет:{summarization}")
 
 
 @dp.message(Command("summarize"))
-async def summarize(message: types.Message,
-                    len_messages=len(participants_messages)):
-    last_participants_messages = participants_messages[len_messages::]
-    participants_messages_str = ''.join(last_participants_messages)
-    summarization = gpt4_interface(
-        f"{MESSAGE_SUMMARIZATION_TEXT} {participants_messages_str}")
+async def command_summarize(message: types.Message, len_messages):
+    summarize(message, len_messages)
 
-    await bot.send_message(message.chat.id, summarization)
+
+@dp.message(Command("answer_to_question"))
+async def answer_to_question(message: types.Message):
+    answer = gpt4_interface(message.text)
+
+    await bot.send_message(message.chat.id, f"Вопрос:{message.text}\n\nОтвет:{answer}")
 
 
 @dp.message(Command("start"))
@@ -62,10 +72,21 @@ async def command_start_handler(message: Message) -> None:
         await asyncio.sleep(1)
         now = datetime.datetime.now()
         current_time = now.strftime("%H:%M:%S")
+
+
+
         if DEBUG:
             print(current_time)
-        if current_time == TIME_TO_PRINT:
-            await summarize(message)
+
+        # if current_time == TIME_TO_PRINT:
+        if now.minute % TIME_INTERVAL_MIN == 0 and now.second == 0:
+            try:
+                await summarize(message)
+                participants_messages.clear()
+            except exceptions.TelegramBadRequest:
+                print("Telegram server says - Bad Request: message is too long")
+            finally:
+                await asyncio.sleep(5)
 
 
 @dp.message(Command("add_to_list"))
@@ -94,8 +115,7 @@ def save_message(message: types.Message) -> None:
     participants[chat_id].append(message.text)
 
     participants_messages.append(message.text)
-    # # Send a copy of the received message
-    # # await message.send_copy(chat_id=message.chat.id)
+
 
 
 @dp.message()
@@ -107,10 +127,10 @@ async def echo_handler(message: types.Message) -> None:
     """
     try:
         save_message(message)
-        # Send a copy of the received message
-        # await message.send_copy(chat_id=message.chat.id)
-        # await bot.send_message(message.chat.id, ''.join(participants))
-        print(participants_messages)
+
+        if DEBUG:
+            print(participants_messages)
+            print(f"Размер масcива:{len(participants_messages)}")
     except TypeError:
         # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
